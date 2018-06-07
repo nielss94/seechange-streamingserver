e74dacc02e8fd4c24f54bf2bd2beee73e516be17
unknown <ricky54321@live.nl>
2018-06-07 13:19:55 +0200

    encryption decryption with hash

--- app.js
+++ app.js
@@ -3,24 +3,6 @@ const app = express();
 const SHA256 = require('crypto-js/sha256');
 const crypto = require('crypto');
 const cryption = require('./crypto/cryptography');
-
-var msg = "Dit wordt, hopelijk, ge-encrypt";
-console.log("Original state: " + msg);
-
-var encryptedMsg = cryption.encryptText(msg);
-console.log("Encrypted msg: " + encryptedMsg);
-
-var decryptedMsg = cryption.decryptText(encryptedMsg);
-console.log("Decrypted msg: " + decryptedMsg);
-
-// msg 1
-const originalData = "Hallo Rick";
-const originalHash = SHA256(originalData).toString();
-
-// msg 2
-const receivedData = "Hallo Rick";
-const recreatedHash = SHA256(receivedData).toString();
-const SHA256 = require('crypto-js/sha256');
 const AES = require('crypto-js/aes');
 const UTF8 = require('crypto-js/enc-utf8');
 
