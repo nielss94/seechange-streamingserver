3706ed68419602b3b73dbfbd2b8a8cb6ce2069db
unknown <ricky54321@live.nl>
2018-06-07 13:00:04 +0200

    Cryptographymethods

--- app.js
+++ app.js
@@ -1,12 +1,24 @@
 const express = require('express');
 const app = express();
+const SHA256 = require('crypto-js/sha256');
+const crypto = require('crypto');
+const cryption = require('./crypto/cryptography');
 
-const SHA256 = require("crypto-js/sha256");
+var msg = "Dit wordt, hopelijk, ge-encrypt";
+console.log("Original state: " + msg);
 
+var encryptedMsg = cryption.encryptText(msg);
+console.log("Encrypted msg: " + encryptedMsg);
+
+var decryptedMsg = cryption.decryptText(encryptedMsg);
+console.log("Decrypted msg: " + decryptedMsg);
+
+// msg 1
 const originalData = "Hallo Rick";
 const originalHash = SHA256(originalData).toString();
 
-const receivedData = "Hallo Riack";
+// msg 2
+const receivedData = "Hallo Rick";
 const recreatedHash = SHA256(receivedData).toString();
 
 console.log(originalHash);
