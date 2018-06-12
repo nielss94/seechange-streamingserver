6a302c1f6f0736a65f3ae10a4064b2f62467d0e6
unknown <ricky54321@live.nl>
2018-06-12 11:51:10 +0200

    Cryptokeys

--- app.js
+++ app.js
@@ -3,26 +3,22 @@ const app = express();
 const SHA256 = require('crypto-js/sha256');
 const crypto = require('crypto');
 const cryption = require('./crypto/cryptography');
+const keys = require('./crypto/cryptokeys')
 const AES = require('crypto-js/aes');
 const UTF8 = require('crypto-js/enc-utf8');
+const RSA = require('node-rsa');
+const NodeMediaServer = require('node-media-server');
 
-// Symmetric key
-const key = '123';
-
-// Data received from Android App
 const data = "Hallo Rick";
-const hash = SHA256(data).toString();
-const cipherHash = AES.encrypt(hash, key);
+const key = new RSA({ b: 512 });
+const privatekey = new RSA(keys.privateKey);
+console.log(key);
+console.log(privatekey);
 
-const plainHash = AES.decrypt(cipherHash, key).toString(UTF8);
+const digitalSignature = key.sign(data);
+const integrityConfirmed = key.verify(data, digitalSignature);
 
-console.log(hash);
-console.log(plainHash);
-
-if (hash === plainHash) {
-    console.log('Integrity confirmed!');
-} else {
-    console.log('NO INTEGRITY. WHO IS RESPONSIBLE FOR THIS?!?!');
-}
+console.log(digitalSignature);
+console.log(integrityConfirmed);
 
 module.exports = app;
